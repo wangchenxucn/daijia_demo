d9236e4f6e844323545578ca1908174c4508d7eb
wangchenxu <wangchenxucn@gmail.com>
2024-08-31 21:30:07 +0800

    修改配置文件与微信小程序密钥

--- service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigOperator.java
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigOperator.java
@@ -2,19 +2,30 @@ package com.atguigu.daijia.customer.config;
 
 import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.config.WxMaConfig;
 import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WxConfigOperator {
+
+
+    @Autowired
+    private WxConfigProperties wxConfigProperties;
+
     @Bean
     public WxMaService wxMaService() {
 
-        WxMaService config = new WxMaServiceImpl() ;
+        WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
+        config.setAppid(wxConfigProperties.getAppId());
+        config.setSecret(wxConfigProperties.getAppSecret());
 
+        WxMaService wxMaService = new WxMaServiceImpl();
+        wxMaService.setWxMaConfig(config);
 
-        return config;
+        return wxMaService;
     }
 
 }
