b85bf8030e547141395720bc2f222d332d557032
wangchenxu <wangchenxucn@gmail.com>
2024-09-15 02:42:14 +0800

    实现预估订单数据

--- service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/MapServiceImpl.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/MapServiceImpl.java
@@ -1,15 +1,57 @@
 package com.atguigu.daijia.map.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.daijia.map.service.MapService;
+import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.vo.map.DrivingLineVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class MapServiceImpl implements MapService {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @Value("${tencent.map.key}")
+    private String key;
+
+    @Override
+    public DrivingLineVo calculateDrivingLine(CalculateDrivingLineForm calculateDrivingLineForm) {
+
+
+        //使用RestTemplate
+        String url = "https://apis.map.qq.com/ws/direction/v1/driving/?from={from}&to={to}&key={key}";
+
+        Map<String,String> map = new HashMap<>();
+        map.put("from",calculateDrivingLineForm.getStartPointLatitude()+","+calculateDrivingLineForm.getStartPointLongitude());
+        map.put("to", calculateDrivingLineForm.getEndPointLatitude()+","+calculateDrivingLineForm.getEndPointLongitude());
+        map.put("key", key);
+
+        //调用
+        JSONObject forObject = restTemplate.getForObject(url, JSONObject.class, map);
+        int status = forObject.getInteger("status");
+        if(status != 0){
+            throw new RuntimeException();
+        }
+        DrivingLineVo drivingLineVo = new DrivingLineVo();
+        JSONObject route =  forObject.getJSONObject("result").getJSONArray("routes").getJSONObject(0);
+
+        drivingLineVo.setDuration(route.getBigDecimal("duration"));
+        drivingLineVo.setDistance(route.getBigDecimal("distance").divide(new BigDecimal(1000)).setScale(2, RoundingMode.HALF_UP));
+        drivingLineVo.setPolyline(route.getJSONArray("polyline"));
+
+        return drivingLineVo;
+
+    }
 }
--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/FeeRuleServiceImpl.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/FeeRuleServiceImpl.java
@@ -1,8 +1,16 @@
 package com.atguigu.daijia.rules.service.impl;
 
-import com.atguigu.daijia.rules.mapper.FeeRuleMapper;
+import com.alibaba.fastjson.JSON;
+import com.atguigu.daijia.model.form.rules.FeeRuleRequest;
+import com.atguigu.daijia.model.form.rules.FeeRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.FeeRuleResponse;
+import com.atguigu.daijia.model.vo.rules.FeeRuleResponseVo;
 import com.atguigu.daijia.rules.service.FeeRuleService;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +19,35 @@ import org.springframework.stereotype.Service;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class FeeRuleServiceImpl implements FeeRuleService {
 
+    @Autowired
+    private KieContainer kieContainer;
 
+    //计算订单费用
+    @Override
+    public FeeRuleResponseVo calculateOrderFee(FeeRuleRequestForm calculateOrderFeeForm) {
+        //封装输入对象
+        FeeRuleRequest feeRuleRequest = new FeeRuleRequest();
+        feeRuleRequest.setDistance(calculateOrderFeeForm.getDistance());
+        feeRuleRequest.setStartTime(new DateTime(calculateOrderFeeForm.getStartTime()).toString("HH:mm:ss"));
+        feeRuleRequest.setWaitMinute(calculateOrderFeeForm.getWaitMinute());
+
+        // 开启会话
+        KieSession kieSession = kieContainer.newKieSession();
+
+        //封装返回对象
+        FeeRuleResponse feeRuleResponse = new FeeRuleResponse();
+        kieSession.setGlobal("feeRuleResponse", feeRuleResponse);
+        // 设置订单对象
+        kieSession.insert(feeRuleRequest);
+        // 触发规则
+        kieSession.fireAllRules();
+        // 中止会话
+        kieSession.dispose();
+        log.info("计算结果：{}", JSON.toJSONString(feeRuleResponse));
+
+        //封装返回对象
+        FeeRuleResponseVo feeRuleResponseVo = new FeeRuleResponseVo();
+        BeanUtils.copyProperties(feeRuleResponse, feeRuleResponseVo);
+        return feeRuleResponseVo;
+    }
 }
